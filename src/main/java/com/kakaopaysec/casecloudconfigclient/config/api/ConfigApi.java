e3e2242d66bf12fb19e4facd08819111eacf9772
임준영 <a79007714@gmail.com>
2022-08-15 21:03:31 +0900

    feat: spring cloud kafka-bus 기능 구현

--- src/main/java/com/kakaopaysec/casecloudconfigclient/config/api/ConfigApi.java
+++ src/main/java/com/kakaopaysec/casecloudconfigclient/config/api/ConfigApi.java
@@ -2,12 +2,14 @@ package com.kakaopaysec.casecloudconfigclient.config.api;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/v1/config")
 @Slf4j
+@RefreshScope
 @RestController
 public class ConfigApi {
 
